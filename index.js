95bb9f62e007a9c4ef32b5b490371efb67b7989e
Kaziev Nursultan <kazievnursultan5554@gmail.com>
2021-04-03 09:59:12 +0600

    Setted up multer to get and post images

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
+const exitHook = require('async-exit-hook');
 const artist = require('./app/artist')
 const app = express();
 
@@ -7,9 +9,23 @@ const port = 8000;
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('public'));
 
 app.use('/artist', artist);
 
-app.listen(port, ()=>{
-    console.log('server started on ${port} port');
-});
+const run = async () => {
+    await mongoose.connect('mongodb://localhost/musicapi',
+        { useNewUrlParser: true, useUnifiedTopology: true});
+
+    app.listen(port, ()=>{
+        console.log('server started on ${port} port');
+    });
+
+    exitHook(async callback => {
+        await mongoose.disconnect();
+        console.log(' mongoose was disconnected');
+        callback();
+    });
+};
+
+run().catch(e=>console.error(e));
